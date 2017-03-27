52464a5bcd3d0f461fc7ec92e4469f4770f1c289
Mohamed Jamshidy <mm.jamshidy@gmail.com>
2017-03-27 02:13:17 -0600

    dummy test cases.

---
+++ sim/src/test/java/com/mpt/mc/sim/SimulationTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.mpt.mc.sim;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author mohamed
+ *
+ */
+public class SimulationTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.mpt.mc.sim.Simulation#Simulation()}.
+	 */
+	@Test
+	public void testSimulation() {
+		assertTrue( true );
+	}
+
+	/**
+	 * Test method for {@link com.mpt.mc.sim.Simulation#Simulation(com.mpt.mc.sim.Portfolio, double, int, int)}.
+	 */
+	@Test
+	public void testSimulationPortfolioDoubleIntInt() {
+		assertTrue( true );
+	}
+
+	/**
+	 * Test method for {@link com.mpt.mc.sim.Simulation#simulate()}.
+	 */
+	@Test
+	public void testSimulate() {
+		assertTrue( true );
+	}
+
+}
